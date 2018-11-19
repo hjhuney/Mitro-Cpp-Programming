34202f6f0d2ad63901ef5f3564e3d1c4d93d0af3
Jake Huneycutt <hjhuney@gmail.com>
2018-11-19 02:24:34 -0500

    Add files via upload

---
+++ Section6/sizeof.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <climits> 
+
+// make sure you include climits for integer types
+// for floating pt numbers, use <cfloat>
+
+using namespace std;
+
+int main(){
+
+    cout << "sizeof info" << endl;
+    cout << "===========" << endl;
+    cout << "unsigned int: " << sizeof(unsigned int) << " bytes." << endl;
+    cout << "short:" << sizeof(short) << " bytes" << endl;
+    cout << "long: " << sizeof(long) << " bytes." << endl;
+    cout << "long long:" << sizeof(long long) << " bytes" << endl;
+    
+    cout << "===========" << endl;
+    cout << "float: " << sizeof(float) << " bytes." << endl;
+    cout << "double:" << sizeof(double) << " bytes" << endl;
+    cout << "long double: " << sizeof(long double) << " bytes." << endl;
+    
+    // use values defined in <climits>
+    cout << "===========" << endl;
+    cout << "Minmum values: " << endl;
+    cout << "char:" << CHAR_MIN << endl;
+    cout << "int: " << INT_MIN << endl;
+    cout << "short: " << SHRT_MIN << endl;
+    cout << "long:" << LONG_MIN << endl;
+    cout << "long long: " << LLONG_MIN << endl;
+    
+    cout << "===========" << endl;
+    cout << "Maximum values: " << endl;
+    cout << "char:" << CHAR_MAX << endl;
+    cout << "int: " << INT_MAX << endl;
+    cout << "short: " << SHRT_MAX << endl;
+    cout << "long:" << LONG_MAX << endl;
+    cout << "long long: " << LLONG_MAX << endl;
+    
+    // sizeof with variable names
+    
+    cout << "===========" << endl;
+    int age {21};
+    cout << "age is " << sizeof(age) << " bytes." << endl;
+    
+    double wage {22.24};
+    cout << "wage is " << sizeof wage << " bytes." << endl;
+    
+    return 0;
+}
